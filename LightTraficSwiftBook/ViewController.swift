bc01b9a3a042bd7acdacd3a21edbcee2284cb6b6
Denis Stashkov <themoldex@gmail.com>
2023-09-21 22:22:24 +0300

    Initial Commit

---
+++ LightTraficSwiftBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LightTraficSwiftBook
+//
+//  Created by Денис Сташков on 21.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
