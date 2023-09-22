bdc73f6819645734df21e524aaca6c2980fceac2
Denis Stashkov <themoldex@gmail.com>
2023-09-22 14:30:10 +0300

    добавил констрейты и навел красоту

--- LightTraficSwiftBook/ViewController.swift
+++ LightTraficSwiftBook/ViewController.swift
@@ -8,42 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentState: ColorLighter = .green
-    
+    // MARK: - outlets
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
     @IBOutlet weak var startButton: UIButton!
     
-    enum ColorLighter {
+    // MARK: - private properties
+    private var currentState: ColorLighter = .green
+    
+    
+    private enum ColorLighter {
         case red
         case yellow
         case green
     }
     
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLight.frame.size.width = 100
-        redLight.frame.size.height = 100
-        redLight.layer.cornerRadius = 50
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
         redLight.alpha = 0.3
         
-        
-        yellowLight.frame.size.width = 100
-        yellowLight.frame.size.height = 100
-        yellowLight.layer.cornerRadius = 50
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
         yellowLight.alpha = 0.3
         
         greenLight.frame.size.width = 100
         greenLight.frame.size.height = 100
         greenLight.layer.cornerRadius = 50
         greenLight.alpha = 0.3
+        // можно сделать и так
         
         startButton.setTitle("Start", for: .normal)
         startButton.layer.cornerRadius = 20
     }
+    // MARK: - IB action
+    @IBAction func startButtonPressed(_ sender: Any) {
+        changeColor()
+        startButton.setTitle("Next", for: .normal)
+    }
     
+    // MARK: - private func
     private func changeColor() {
         switch currentState {
         case .red:
@@ -61,11 +68,4 @@ class ViewController: UIViewController {
         yellowLight.alpha = currentState == .yellow ? 1 : 0.3
         greenLight.alpha = currentState == .green ? 1 : 0.3
     }
-    
-    @IBAction func startButtonPressed(_ sender: Any) {
-        changeColor()
-        startButton.setTitle("Next", for: .normal)
-    }
-
 }
-
